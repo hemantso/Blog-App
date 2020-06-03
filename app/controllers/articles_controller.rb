c42d672819ca3891366a5e8fe2d892215f964039
Hemant soni <sonihemant4741@gmail.com>
2020-06-03 22:37:56 +0530

    rubocop error resolved

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,30 +3,35 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
+
   def show
     @article = Article.find(params[:id])
     @comment = Comment.new
     @comment.article_id = @article.id
-
   end
+
   def new
     @article = Article.new
   end
+
   def create
-    @article = Article.new(article_params)    
+    @article = Article.new(article_params)
     @article.save
     flash.notice = "Article '#{@article.title}' Created!"
     redirect_to article_path(@article)
   end
+
   def destroy
     @article = Article.find(params[:id])
     @article.destroy
     flash.notice = "Article '#{@article.title}' Deleted!"
     redirect_to articles_path
   end
-  def edit 
+
+  def edit
     @article = Article.find(params[:id])
   end
+
   def update
     @article = Article.find(params[:id])
     @article.update(article_params)
@@ -34,5 +39,4 @@ class ArticlesController < ApplicationController
 
     redirect_to article_path(@article)
   end
-  
 end
